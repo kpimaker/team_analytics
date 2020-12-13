02a3ab198046f743e0f697e8ea1134d9eb5a041e
kbashevoy <kbashevoy@yandex-team.ru>
2020-12-13 20:25:46 +0300

    Добавлен отчет по продажам в разбивке по источникам трафика

---
+++ sales_by_sources/main.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+import pandas as pd
+import sqlite3
+
+
+def sales_by_sources_report(connection):
+    """
+    Формирует отчет по продажам в разбивке по источникам трафика.
+    Возвращает pandas dataframe
+    """
+
+    sql_query = """
+        SELECT source, purchases 
+        FROM (
+            SELECT source, purchases from visits_grouped
+            INNER JOIN customers ON visits_grouped.user_id = customers.user_id
+        )
+        GROUP BY source
+        ORDER BY purchases DESC;
+    """
+
+    return pd.read_sql(sql_query, connection)
+
+
+def main():
+    connection = sqlite3.connect('../ecommerce.db')
+
+    report = sales_by_sources_report(connection)
+    print(report)
+
+
+if __name__ == '__main__':
+    main()
